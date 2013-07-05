8f5e85dca5191385ad94ffb2f06cc452fd232437
Jurgen Puype <jurgen.puype@skynet.be>
2013-07-05 16:52:52 +0000

    Corrected buffer

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 require('fs');OAAOA
 var express = require('express');
 
-var buf = new Buffer(fs.readFileSync('index.html'));
-var txt = buf.toString();
+//var buf = new Buffer(fs.readFileSync('index.html'));
+//var txt = buf.toString();
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(txt);
+  response.send(fs.readFileSync('index.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;

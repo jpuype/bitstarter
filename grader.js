7311c387544d700406166bdd73e01fe946ef51fb
Jurgen Puype <jurgen.puype@skynet.be>
2013-07-08 11:14:33 +0000

    changes to grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -34,6 +35,25 @@ var assertFileExists = function(infile) {
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var gradeUrl = function(result, response) {
+    if (result instanceof Error) {
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    } else {
+	fs.writeFileSync(HTMLFILE_DEFAULT, result);
+	var checkJson = checkHtmlFile(HTMLFILE_DEFAULT, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }   
+}
+
+var getUrl = function(url) {
+/*    console.log(url);
+    var arr = url.split(" ");
+    console.log(arr[1] + "/" + arr[0]);
+    var _url = arr[1] + "/" + arr[0];
+*/    rest.get(url).on('complete', gradeUrl);
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,14 +75,32 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var clone = function(fn) {
+	// Workaround for commander.js issue.
+	// http://stackoverflow.com/a/6772648
+	return fn.bind({});
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <htmm_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_uri>', 'Url of the HTML-file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var url = program.url;
+    if(typeof program.url != 'undefined') {
+	console.log('Url parameter is present');
+	getUrl(program.url);
+    } else {
+	console.log('Url parameter is NOT present');
+	// var pfile = program.file;
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+//    var checkJson = checkHtmlFile(pfile, program.checks);
+//    var outJson = JSON.stringify(checkJson, null, 4);
+//    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
